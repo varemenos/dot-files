78c46752833636319fea51cf3fb4f588732b928f
Adonis Kakoulidis <aklp08@gmail.com>
2024-06-13 15:33:21 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -90,3 +90,10 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# pnpm
+set -gx PNPM_HOME "/Users/kakoulidis/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
